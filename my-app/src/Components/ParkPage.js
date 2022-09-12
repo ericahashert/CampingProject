f4ecf2e5b7348a94d9109945914b792c68d7ed3c
Edward Dickey <109627719+edwarddickey@users.noreply.github.com>
2022-09-12 15:55:53 -0600

    Update ParkPage.js

--- my-app/src/Components/ParkPage.js
+++ my-app/src/Components/ParkPage.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import ParkList from './ParkList';
+import Search from './Search';
 
-function ParkPage () {
-    fetch("https://developer.nps.gov/api/v1/")
-        .then(r=>r.json())
-        .then(data=>console.log(data))
+function ParkPage() {
+    const [parks, setParks] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
+    const displayedParks = parks.filter((park) => {
+        return park.name.toLowerCase().includes(searchTerm.toLowerCase());
+        });
+  
+    useEffect(() => {
+      fetch("https://developer.nps.gov/api/v1/parks?parkCode=&api_key=j1k4pib60NZrUWACuiahvKfh9s7O7BOhHRr4qoGG")
+        .then((r) => r.json())
+        .then((parksArray) => {
+          //setParks(parksArray);
+          console.log(parksArray);
+        });
+    }, []);
+return (
+    <div>
+        <Search searchTerm={searchTerm} onSearchChange={setSearchTerm}/>
+      <ParkList
+        parks={displayedParks}
+        />
+    </div>
+)
+
 }
+
+export default ParkPage;

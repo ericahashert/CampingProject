83f0d5aee0bc6a678fbe6f54acef157091f03e8c
Edward Dickey <109627719+edwarddickey@users.noreply.github.com>
2022-09-12 15:54:51 -0600

    Update ParkList.js

---
+++ my-app/src/Components/ParkList.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ParkCard from './ParkCard';
+import Search from './Search';
+
+function ParkList ({parks}){
+    
+    return(
+        <div>
+        
+             {parks.map((park) => {
+        return (
+          <ParkCard
+            key={park.id}
+            park={park}
+            />
+            );
+      })}
+         </div>
+    )
+}
+
+export default ParkList;

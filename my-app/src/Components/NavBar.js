339dc3181b31acf0b254533b5e16ba1cbfc6a06c
Edward Dickey <109627719+edwarddickey@users.noreply.github.com>
2022-09-12 15:54:00 -0600

    Update NavBar.js

---
+++ my-app/src/Components/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function NavBar (){
+    return (
+        <div className="nav">
+            <p>Park Map</p>
+            <p>Park List</p>
+            <p>Photo Gallery</p>
+        </div>
+    )
+}
+
+export default NavBar;

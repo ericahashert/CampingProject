72c1d4053508594b564e8fc8e8091f73325a6927
aspencass <caspenw1@gmail.com>
2022-09-13 13:58:58 -0600

    afternoon update

--- my-app/src/Components/App.js
+++ my-app/src/Components/App.js
@@ -8,8 +8,8 @@ function App() {
   return (
     <div className="app">
       <Header />
-      <ParkPage />
       <NavBar />
+      <ParkPage />
     </div>
   );
 }
--- my-app/src/Components/ParkCard.js
+++ my-app/src/Components/ParkCard.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import { BackpackRounded } from '@mui/icons-material';
+import React, { useState } from 'react';
 
 function ParkCard({image, name}) {
 
+    const [showFront, setShowFront] = useState(true)
+
+    function frontCard() {
+        return (
+        <img className="card-image" src={image} />
+        )
+    }
+
+    function backCard() {
+        return(
+        <img className="card-image" src="" />
+        )
+    }
+
+    function handleClick() {
+        setShowFront((showFront) => !showFront)
+      }
+    
 
     return (
-        <div className = "card">
-            <img className ="card-image" src={image} alt={name} />
-            <div className="container">
+        <div onClick={handleClick}>
             <h4 className='card-title'>{name}</h4>
+            <div className="container">
+            {showFront ? frontCard() : backCard()}
             </div>
         </div>
     );

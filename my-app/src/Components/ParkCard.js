560ab325296c7a981488279b486c28d3b2c3f7ba
Erica Hashert <ericahashert@gmail.com>
2022-09-14 09:28:50 -0600

    styling on homepage

--- my-app/src/Components/ParkCard.js
+++ my-app/src/Components/ParkCard.js
@@ -1,25 +1,47 @@
 import React, {useState} from 'react';
 
-function ParkCard({image, name, description, activities}) {
+function ParkCard({image, name, description, activities, admission, addressStreet, addressCity, addressState, addressZip}) {
     const [showFront, setShowFront] = useState(true);
-    console.log(activities[0])
+    //const [image, setImage] = useState("")
+
+
     if (activities === undefined) return <p>LOADING...</p>
+
+    // function handleClickEvent () {
+    //     if (image1 === true) {
+    //         return image2
+    //     } if (image2 === true) {
+    //         return image3
+    //     } if (image3 === true) {
+    //         return image4
+    //     } if (image4 === true) {
+    //         return image1
+    //     }
+    // }
 
     function frontCard() {
         return (
-            <img className ="card-image" src={image}/>
+            <li className="frontCard">
+                <img className ="card-image" src={image}/>
+                <p>Admission: ${admission}</p>
+                <div className ="address">
+                    <p>Address: {addressStreet}</p>
+                    <p>{addressCity}, {addressState} {addressZip}</p>
+                </div>
+            </li>
         )
     }
     function backCard() {
         return (
             <div>
                 <p>{description}</p>
-                <p> Activities Include: {activities[0].name} and {activities[1].name}</p>
-                
-                
-                
-                
-
+                <p> Activities Include:</p>
+                    <ul>
+                        <li>{activities[0].name}</li>
+                        <li>{activities[1].name}</li>
+                        <li>{activities[2].name}</li>
+                        <li>{activities[3].name}</li>
+                    </ul>
             </div>
         )
     }
--- my-app/src/Components/ParkList.js
+++ my-app/src/Components/ParkList.js
@@ -11,8 +11,16 @@ function ParkList ({ parks }){
           key={park.id}
           name={park.fullName}
           image={park.images[0].url}
+          // image2={park.images[1].url}
+          // image3={park.images[2].url}
+          // image4={park.images[3].url}
           description={park.description}
           activities ={park.activities}
+          admission = {park.entranceFees.cost}
+          addressStreet = {park.addresses[0].line1}
+          addressCity = {park.addresses[0].city}
+          addressState = {park.addresses[0].stateCode}
+          addressZip = {park.addresses[0].postalCode}
         />)
   
     return (
